7886d095265603a4b8fa5009ead28c6fab8945a5
Yaelsobrido <yaelsobrido23@gmail.com>
2025-04-01 17:23:29 +0300

    feature: switch database from PostgreSQL to MySQL and add dotenv support

--- knexfile.ts
+++ knexfile.ts
@@ -1,13 +1,17 @@
 import type { Knex } from "knex";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "pg",
+    client: "mysql",
     connection: {
-      database: process.env.POSTGRES_DB || "nodejs-db",
-      user: process.env.POSTGRES_USER || "user",
-      password: process.env.POSTGRES_PASSWORD || "1234",
-      host: process.env.POSTGRES_HOST || "localhost",
+      database: process.env.DB_DATABASE || "employee",
+      user: process.env.DB_USERNAME || "root",
+      password: process.env.DB_PASSWORD || "",
+      host: process.env.DB_HOST || "127.0.0.1",
+      port: parseInt(process.env.DB_PORT || "3306", 10),
     },
     migrations: {
       directory: './db/migrations',
