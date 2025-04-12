8b8d1e10de6189f7ca856e4e3e0e4bbc4352969b
annaick <annaick.tiana@gmail.com>
2025-04-12 08:50:45 +0300

    changed header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return(
         <header className="w-full grid grid-cols-1 md:grid-cols-[1fr_400px] gap-[32px]">
             <div className="flex justify-between items-center">
-                <h1 className="font-extrabold text-2xl underline-offset-4">Projet NodeJS</h1>
+                <h1 className="font-extrabold text-2xl underline-offset-4">Gestion des employés</h1>
                 <ThemeToggle />
             </div>
         </header>
